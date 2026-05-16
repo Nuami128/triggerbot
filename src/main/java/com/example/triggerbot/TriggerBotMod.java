b79819b2e63d58ea54d63369d719634dd9c1029b
Nuami128 <ahmadhazhir@outlook.com>
2026-05-16 08:26:50 +0100

    Update TriggerBotMod.java

--- src/main/java/com/example/triggerbot/TriggerBotMod.java
+++ src/main/java/com/example/triggerbot/TriggerBotMod.java
@@ -1,1 +1,12 @@
+package com.example.triggerbot;
 
+import net.fabricmc.api.ModInitializer;
+
+public class TriggerBotMod implements ModInitializer {
+
+    public static boolean attackAssist = true;
+
+    @Override
+    public void onInitialize() {
+    }
+}
