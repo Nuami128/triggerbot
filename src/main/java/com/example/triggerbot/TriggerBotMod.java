95b8f8a6ff14a3da789d9688c34de01c8652e3db
Nuami128 <ahmadhazhir@outlook.com>
2026-05-16 18:45:33 +0100

    Restore original build tooling files

--- src/main/java/com/example/triggerbot/TriggerBotMod.java
+++ src/main/java/com/example/triggerbot/TriggerBotMod.java
@@ -1,12 +1,17 @@
 package com.example.triggerbot;
 
+import com.example.triggerbot.module.ModuleManager;
 import net.fabricmc.api.ClientModInitializer;
 
 public class TriggerBotMod implements ClientModInitializer {
-
-    public static boolean attackAssist = true;
+    private static final ModuleManager MODULE_MANAGER = new ModuleManager();
 
     @Override
     public void onInitializeClient() {
+        MODULE_MANAGER.initialize();
+    }
+
+    public static ModuleManager getModuleManager() {
+        return MODULE_MANAGER;
     }
 }
