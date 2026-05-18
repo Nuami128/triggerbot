e28c9c8d6c6e229ddfdce4f30f66bde532d52df9
Nuami128 <ahmadhazhir@outlook.com>
2026-05-18 06:20:54 +0100

    Update AutoStunModule.java

--- src/main/java/com/example/triggerbot/module/impl/AutoStunModule.java
+++ src/main/java/com/example/triggerbot/module/impl/AutoStunModule.java
@@ -8,7 +8,7 @@ import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.entity.player.PlayerInventory;
 import net.minecraft.item.AxeItem;
 import net.minecraft.item.ItemStack;
-import net.minecraft.item.SwordItem;
+import net.minecraft.world.item.SwordItem;
 import net.minecraft.network.packet.c2s.play.UpdateSelectedSlotC2SPacket;
 import net.minecraft.util.Hand;
 
