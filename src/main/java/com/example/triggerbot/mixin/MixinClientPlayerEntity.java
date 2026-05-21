91eba2fa66f37389444c84ab3ba364ca796ca55e
Nuami128 <ahmadhazhir@outlook.com>
2026-05-21 06:01:42 +0100

    Create MixinClientPlayerEntity.java

---
+++ src/main/java/com/example/triggerbot/mixin/MixinClientPlayerEntity.java
@@ -0,0 +1,17 @@
+package com.example.triggerbot.mixin;
+
+import com.example.triggerbot.module.ModuleManager;
+import net.minecraft.client.network.ClientPlayerEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(ClientPlayerEntity.class)
+public class MixinClientPlayerEntity {
+
+    @Inject(method = "sendMovementPackets", at = @At("TAIL"))
+    private void onPostMovement(CallbackInfo ci) {
+        ModuleManager.getInstance().postMovementAll();
+    }
+}
