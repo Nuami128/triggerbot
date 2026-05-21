25c2183ef717dd2ce672ba501a04833f2c45fccd
Nuami128 <ahmadhazhir@outlook.com>
2026-05-21 06:28:55 +0100

    Update MixinClientPlayerEntity.java

--- src/main/java/com/example/triggerbot/mixin/MixinClientPlayerEntity.java
+++ src/main/java/com/example/triggerbot/mixin/MixinClientPlayerEntity.java
@@ -1,17 +1,21 @@
 package com.example.triggerbot.mixin;
 
 import com.example.triggerbot.module.ModuleManager;
-import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
+import net.minecraft.network.packet.Packet;
+import net.minecraft.network.packet.c2s.play.PlayerMoveC2SPacket;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
-@Mixin(ClientPlayerEntity.class)
+@Mixin(ClientPlayNetworkHandler.class)
 public class MixinClientPlayerEntity {
 
-    @Inject(method = "sendMovementPackets", at = @At("TAIL"))
-    private void onPostMovement(CallbackInfo ci) {
-        ModuleManager.getInstance().postMovementAll();
+    @Inject(method = "sendPacket*", at = @At("HEAD"))
+    private void onSendPacket(Packet<?> packet, CallbackInfo ci) {
+        if (packet instanceof PlayerMoveC2SPacket) {
+            ModuleManager.getInstance().postMovementAll();
+        }
     }
 }
