3439fb6fd121555c9e2885b3ae84d66703b127ef
Nuami128 <ahmadhazhir@outlook.com>
2026-05-23 21:15:49 +0100

    Update TriggerBotMod.java

--- src/main/java/com/example/triggerbot/TriggerBotMod.java
+++ src/main/java/com/example/triggerbot/TriggerBotMod.java
@@ -3,6 +3,7 @@ package com.example.triggerbot;
 import com.example.triggerbot.module.ModuleManager;
 import com.example.triggerbot.module.impl.AutoSprintModule;
 import com.example.triggerbot.module.impl.AutoStunModule;
+import com.example.triggerbot.module.impl.InventoryEatModule;
 import com.example.triggerbot.module.impl.TriggerBotModule;
 
 import net.fabricmc.api.ClientModInitializer;
@@ -14,6 +15,7 @@ public class TriggerBotMod implements ClientModInitializer {
     private static AutoStunModule AUTO_STUN;
     private static TriggerBotModule TRIGGER;
     private static AutoSprintModule AUTO_SPRINT;
+    private static InventoryEatModule INVENTORY_EAT;
 
     @Override
     public void onInitializeClient() {
@@ -22,14 +24,17 @@ public class TriggerBotMod implements ClientModInitializer {
         AUTO_STUN = new AutoStunModule();
         TRIGGER = new TriggerBotModule(AUTO_STUN);
         AUTO_SPRINT = new AutoSprintModule();
+        INVENTORY_EAT = new InventoryEatModule();
 
         MODULE_MANAGER.register(AUTO_STUN);
         MODULE_MANAGER.register(TRIGGER);
         MODULE_MANAGER.register(AUTO_SPRINT);
+        MODULE_MANAGER.register(INVENTORY_EAT);
 
         // Always on
         TRIGGER.onEnable();
         AUTO_SPRINT.onEnable();
+        INVENTORY_EAT.onEnable();
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             MODULE_MANAGER.tickAll();
