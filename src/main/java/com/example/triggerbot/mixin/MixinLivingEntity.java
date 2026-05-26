57b8c6d3494bd5c4401bcfc178b1f69d6a0faca8
Nuami128 <ahmadhazhir@outlook.com>
2026-05-26 07:47:38 +0100

    Update MixinLivingEntity.java

--- src/main/java/com/example/triggerbot/mixin/MixinLivingEntity.java
+++ src/main/java/com/example/triggerbot/mixin/MixinLivingEntity.java
@@ -5,6 +5,7 @@ import com.example.triggerbot.module.impl.AutoJumpResetModule;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.server.world.ServerWorld;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -14,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class MixinLivingEntity {
 
     @Inject(method = "damage", at = @At("HEAD"))
-    private void onDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+    private void onDamage(ServerWorld world, DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
         MinecraftClient mc = MinecraftClient.getInstance();
         if (mc.player == null) return;
         if ((Object) this != mc.player) return;
