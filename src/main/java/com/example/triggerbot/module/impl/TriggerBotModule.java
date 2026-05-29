1bb019e78c3be8aa2eb3367c2bf6f4d0f2a7cc68
Nuami128 <ahmadhazhir@outlook.com>
2026-05-29 15:25:27 +0100

    Update TriggerBotModule.java

--- src/main/java/com/example/triggerbot/module/impl/TriggerBotModule.java
+++ src/main/java/com/example/triggerbot/module/impl/TriggerBotModule.java
@@ -158,7 +158,6 @@ public class TriggerBotModule implements ClientModule {
             mc.player.swingHand(Hand.MAIN_HAND);
             cooldownTicks = 1;
             autoSprint.onAttack();
-            ((net.minecraft.client.network.ClientPlayerEntity) mc.player).sendMovementPackets();
         }
     }
 
