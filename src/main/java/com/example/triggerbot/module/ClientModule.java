e1f6bd7b3895559887da86f5ee2d8215851cf1c9
Nuami128 <ahmadhazhir@outlook.com>
2026-05-31 16:52:00 +0100

    Update ClientModule.java

--- src/main/java/com/example/triggerbot/module/ClientModule.java
+++ src/main/java/com/example/triggerbot/module/ClientModule.java
@@ -1,20 +1,12 @@
 package com.example.triggerbot.module;
 
 public interface ClientModule {
-
     String getName();
-
-    void onEnable();
-
-    void onDisable();
-
-    void onTick();
-
-    void onPostMovement();
-
-    void onJumpReset();
-
-    default void onDamage() {}
-
     boolean isEnabled();
+    void onEnable();
+    void onDisable();
+    void onTick();
+    void onPostMovement();
+    void onJumpReset();
+    void onDamage(); // ADD THIS
 }
