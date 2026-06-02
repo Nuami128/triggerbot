b69636ef732e8f76216cd08cca5ed73ff509ac37
Nuami128 <ahmadhazhir@outlook.com>
2026-06-02 20:33:30 +0100

    Update PlayerAttackMixin.java

--- src/main/java/com/example/triggerbot/mixins/PlayerAttackMixin.java
+++ src/main/java/com/example/triggerbot/mixins/PlayerAttackMixin.java
@@ -2,17 +2,16 @@ package com.example.triggerbot.mixin;
 
 import com.example.triggerbot.TriggerBotMod;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.entity.Entity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(MinecraftClient.class)
 public class PlayerAttackMixin {
 
     @Inject(method = "doAttack", at = @At("HEAD"))
-    private void onAttack(CallbackInfo ci) {
+    private void onAttack(CallbackInfoReturnable<Boolean> cir) {
         TriggerBotMod.getModuleManager().onAttackAll();
     }
 }
