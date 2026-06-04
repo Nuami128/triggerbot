a70a8a99c81fa1963cea3280b04295ef9054cfe0
Nuami128 <ahmadhazhir@outlook.com>
2026-06-04 18:28:10 +0100

    Update TriggerBotModule.java

--- src/main/java/com/example/triggerbot/module/impl/TriggerBotModule.java
+++ src/main/java/com/example/triggerbot/module/impl/TriggerBotModule.java
@@ -71,6 +71,7 @@ public class TriggerBotModule implements ClientModule {
 
     @Override
     public void onPostMovement() {
+        if (true) return; // temporary test
         MinecraftClient mc = MinecraftClient.getInstance();
 
         if (!enabled) return;
