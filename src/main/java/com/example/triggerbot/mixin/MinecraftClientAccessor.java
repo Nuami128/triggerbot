5a8ba235013483a91d74b37509d35ed731cd918e
Nuami128 <ahmadhazhir@outlook.com>
2026-06-04 21:34:39 +0100

    Create MinecraftClientAccessor.java

---
+++ src/main/java/com/example/triggerbot/mixin/MinecraftClientAccessor.java
@@ -0,0 +1,12 @@
+package com.example.triggerbot.mixin;
+
+import net.minecraft.client.MinecraftClient;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Invoker;
+
+@Mixin(MinecraftClient.class)
+public interface MinecraftClientAccessor {
+    @Invoker("doAttack")
+    boolean invokeDoAttack();
+}
+
